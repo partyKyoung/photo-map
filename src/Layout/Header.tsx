4a8e6189fc1d7043fba9dc69cf12b69ee4c8c0df
sunrereck <aet789@naver.com>
2019-12-10 01:38:20 +0900

    feat:  헤더, 사이드바 작업

---
+++ src/Layout/Header.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+
+type HeaderProps = {
+  children?: string;
+  open: boolean;
+  setOpen: (open: boolean) => void;
+};
+
+const StyledHeader = styled.div`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 50px;
+  text-align: center;
+  background: linear-gradient(
+    90deg,
+    #d4dfe6,
+    #8ec0e4 35%,
+    #cadbe9 69%,
+    #6aafe6
+  );
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledHeaderText = styled.div`
+  font-size: 20px;
+`;
+
+const StyledMenuButton = styled.button`
+  position: fixed;
+  right: 0;
+`;
+
+function Header({ open, setOpen }: HeaderProps) {
+  return (
+    <StyledHeader>
+      <StyledHeaderText>헤더</StyledHeaderText>
+      <StyledMenuButton onClick={() => setOpen(!open)}>
+        <img src="../image/bar-menu.svg" alt="메뉴버튼" />
+      </StyledMenuButton>
+    </StyledHeader>
+  );
+}
+
+export default Header;
---
+++ src/Layout/SideBar.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import styled from 'styled-components';
+
+type SideBarProps = {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+};
+
+type StyleProps = {
+  open: boolean;
+};
+
+const StyledSideBar = styled.nav<StyleProps>`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background: #e8e8e9;
+  height: 100vh;
+  text-align: left;
+  padding: 0 2rem;
+  position: absolute;
+  top: 0;
+  right: 0;
+  transition: transform 0.3s ease-in-out;
+  transform: ${({ open }) => (open ? 'translateX(0)' : 'translateX(100%)')};
+
+  a {
+    font-size: 2rem;
+    text-transform: uppercase;
+    padding: 2rem 0;
+    font-weight: bold;
+    letter-spacing: 0.5rem;
+    text-decoration: none;
+    transition: color 0.3s linear;
+  }
+`;
+
+const StyledMenuButton = styled.button`
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+function SideBar({ open, setOpen }: SideBarProps) {
+  return (
+    <StyledSideBar open={open}>
+      <StyledMenuButton onClick={() => setOpen(!open)}>
+        <img src="../image/bar-menu.svg" alt="메뉴버튼" />
+      </StyledMenuButton>
+      <a href="/">
+        <span role="img" aria-label="about us">
+          &#x1f481;&#x1f3fb;&#x200d;&#x2642;&#xfe0f;
+        </span>
+        내사진
+      </a>
+      <a href="/">
+        <span role="img" aria-label="price">
+          &#x1f4b8;
+        </span>
+        니사진
+      </a>
+      <a href="/">
+        <span role="img" aria-label="contact">
+          &#x1f4e9;
+        </span>
+        우리사진
+      </a>
+    </StyledSideBar>
+  );
+}
+
+export default SideBar;
