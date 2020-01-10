2ffdab37a1afa14c3c43edfb1fe6e22b5fc95336
partyKyoung <cathy.park@bunjang.co.kr>
2020-01-10 19:16:37 +0900

    feat: alert 모달 작성 중

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import Alert from '@/components/ui/Alert';
 import PageTemplate from '@/components/templates/PageTemplate';
 
 const Home = () => {
-  return <PageTemplate>임시 홈</PageTemplate>;
+  return (
+    <PageTemplate>
+      임시 홈
+      {/* <Alert onClick={() => {}} onClose={() => {}}>
+        풍월량
+      </Alert> */}
+    </PageTemplate>
+  );
 };
 
 export default Home;
