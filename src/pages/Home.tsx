053c0878c6a26f780f9275e9be85726bb62ba849
partyKyoung <cathy.park@bunjang.co.kr>
2020-01-13 14:20:22 +0900

    chore: Alert 모달 소스 삭제 - 추후에 다시 만들 예정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
 
-import Alert from '@/components/ui/Alert';
 import PageTemplate from '@/components/templates/PageTemplate';
 
 const Home = () => {
-  return (
-    <PageTemplate>
-      임시 홈
-      {/* <Alert onClick={() => {}} onClose={() => {}}>
-        풍월량
-      </Alert> */}
-    </PageTemplate>
-  );
+  return <PageTemplate>임시 홈</PageTemplate>;
 };
 
 export default Home;
